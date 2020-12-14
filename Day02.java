f9e0788351c6bf3fbf81a55d57dbdfbf70e24a83
MG <62848619+mgazdovic@users.noreply.github.com>
2020-12-15 00:20:42 +0100

    Update Day02.java
    Clean up

--- Day02.java
+++ Day02.java
@@ -1,96 +1,51 @@
+/**
+ * Advent Of Code 2020
+ *  >>> www.adventofcode.com/2020
+ * 
+ * This file contains utility methods to fetch and validate input data. 
+ * 
+ * @author Mislav.Gazdovic, mislav.gazdovic@gmail.com
+ *
+ */
+
 package adventOfCode2020;
 
 import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class Day02 {
-
-	public static int pwdRuleLetterOccurences(Collection<String> passwords) {
-		
-		int passwordOkCount = 0;
-		
-		int minCount, maxCount;
-		char letter;
-		String password;
-		int dashIndex = 0;
-		int colonIndex = 0;
-		for(String item : passwords) {
-			
-			// parse string to get rule details, for example: 
-			// 9-10 g: bgbgjggpvgpgpggg
-			
-			dashIndex = item.indexOf('-');
-			colonIndex = item.indexOf(':');
-			
-			minCount = Integer.parseInt(item.substring(0, dashIndex));
-			maxCount = Integer.parseInt(item.substring(dashIndex + 1, colonIndex - 2));
-			letter = item.charAt(colonIndex - 1);
-			password = item.substring(colonIndex + 2);
-			
-			// count the number of letter instances within item
-			int count = 0;
-			for (int i = 0; i < password.length(); i++) {
-				if (password.charAt(i) == letter) count++;
+public class Util {
+	
+	/**
+	* Return a string collection (one item per row) from file at specified path. 
+	*
+	* @param path  Relative path to the input file
+	* @throws IOException if reading file is unsuccessful
+	*/
+	public static Collection<String> getInputAsStringCollection(String path) {
+		try {
+			Collection<String> input = new ArrayList<String>();
+			for (String row : Files.readAllLines(Paths.get(path)))
+			{
+				input.add(row);
 			}
-			
-			if (count >= minCount && count <= maxCount) {
-				passwordOkCount++;
-			}		
+			return input;
+		} catch (IOException e) {
+			System.out.format("Error reading input file: \n%s\n", e.getMessage());
+			e.printStackTrace();
+			return null;
 		}
-		
-		return passwordOkCount;
 	}
 	
-	public static int pwdRuleLetterPositions(Collection<String> passwords) {
-		
-		int passwordOkCount = 0;
-		
-		int firstPosition, secondPosition;
-		char firstLetter, secondLetter, letter;
-		String password;
-		int dashIndex = 0;
-		int colonIndex = 0;
-		for(String item : passwords) {
-			
-			// parse string to get rule details, for example: 
-			// 9-10 g: bgbgjggpvgpgpggg
-			
-			dashIndex = item.indexOf('-');
-			colonIndex = item.indexOf(':');
-			
-			password = item.substring(colonIndex + 2);
-			
-			firstPosition = Integer.parseInt(item.substring(0, dashIndex));			
-			firstLetter = password.charAt(firstPosition - 1); 		// one-based position -> zero-based index
-			
-			secondPosition = Integer.parseInt(item.substring(dashIndex + 1, colonIndex - 2));
-			secondLetter = password.charAt(secondPosition - 1); 	// one-based position -> zero-based index
-			
-			letter = item.charAt(colonIndex - 1);
-			
-			// exactly one letter has to match for the password to be OK
-			if ((firstLetter == letter) ^ (secondLetter == letter)) {
-				passwordOkCount++;
-			}		
+	/**
+	* Checks if input collection contains any elements. 
+	*
+	* @throws IllegalArgumentException if input collection is null or empty.
+	*/
+	public static void notEmptyOrThrow(Collection<?> collection) {
+		if (collection == null || collection.size() == 0) {
+			throw new IllegalArgumentException("Collection has to contain at least one element...");
 		}
-		
-		return passwordOkCount;
 	}
-		
-	private static Collection<String> getInput() {
-		List<String> stringList = new ArrayList<String>();
-		stringList.addAll(Util.getInputAsStringCollection("Inputs\\Day02.txt"));
-		return stringList;
-	}
-	
-	// Driver
-	public static void main(String[] args) {
-		
-		final Collection<String> input = getInput();
-			
-		System.out.printf("The number of correct passwords for occurence rule is %d.", pwdRuleLetterOccurences(input));	// PART 1
-		System.out.println();
-		System.out.printf("The number of correct passwords for position rule is %d.", pwdRuleLetterPositions(input));	// PART 2
-		
-	}
-
 }
