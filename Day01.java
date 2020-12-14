233c736a99a9142023d893c4f51006e3518488e9
MG <62848619+mgazdovic@users.noreply.github.com>
2020-12-15 00:20:27 +0100

    Update Day01.java
    Clean up

--- Day01.java
+++ Day01.java
@@ -1,55 +1,64 @@
+/**
+ * Advent Of Code 2020
+ *  >>> www.adventofcode.com/2020
+ * 
+ * This file contains solutions to day 1.
+ * 
+ * @author Mislav.Gazdovic, mislav.gazdovic@gmail.com
+ *
+ */
+
 package adventOfCode2020;
 
-import java.util.*;
-import java.io.IOException;
+import java.util.HashSet;
 
 public class Day01 {
 	
-	public static long TwoSum(HashSet<Integer> input, int targetSum) {
-		for (int num : input)
-		{
-			if (input.contains(targetSum - num))
-				return num*(targetSum - num);
-		}
-				
-		throw new IllegalArgumentException("The input does not contain targetSum!");
-	}
 	
-	public static long ThreeSum(HashSet<Integer> input, int targetSum) {	
-		for (int num : input)
-		{
-			try {
-				return num * TwoSum(input, targetSum - num);
-			}
-			catch (IllegalArgumentException e) {
-				continue;
+	public static Long TwoSum(HashSet<Long> numSet, long targetSum) {
+		Util.notEmptyOrThrow(numSet);
+		if (numSet.size() == 1) return null;
+			
+		for (long num : numSet) {
+			if (numSet.contains(targetSum - num)) {
+				return Long.valueOf(num*(targetSum - num));
 			}
 		}
 		
-		throw new IllegalArgumentException("The input does not contain targetSum!");
+		return null;
 	}
 	
-	private static HashSet<Integer> getInput() {
-		HashSet<Integer> numSet = new HashSet<Integer>();
-		for (String item : Util.getInputAsStringCollection("Inputs\\Day01.txt"))
-		{
-			numSet.add(Integer.parseInt(item));
+	
+	public static Long ThreeSum(HashSet<Long> numSet, long targetSum) {	
+		Util.notEmptyOrThrow(numSet);
+		
+		Long twoSum;
+		for (long num : numSet) {
+			twoSum = TwoSum(numSet, targetSum - num);
+			if (twoSum == null) continue;
+			return num * twoSum;
 		}
+		
+		return null;
+	}
+	
+	private static HashSet<Long> getInput() {
+		HashSet<Long> numSet = new HashSet<Long>();
+		
+		for (String item : Util.getInputAsStringCollection("Inputs\\Day01.txt")) {
+			numSet.add(Long.parseLong(item));
+		}
+		
 		return numSet;
 	}
-	
 		
 	// Driver
-	public static void main(String[] args) throws IOException {
-
-		final int TARGET = 2020;
-		final HashSet<Integer> input = getInput();
+	public static void main(String[] args) {
+		Long targetSum = 2020L;
+		HashSet<Long> numSet = getInput();
 		
-		System.out.printf("The TwoSum result is %d. ", TwoSum(input, TARGET));		// PART 1
-		System.out.println();
-		System.out.printf("The ThreeSum result is %d. ", ThreeSum(input, TARGET));	// PART 2
+		System.out.printf("The TwoSum result is %d.\n", TwoSum(numSet, targetSum));	// PART 1
+		System.out.printf("The ThreeSum result is %d. ", ThreeSum(numSet, targetSum));	// PART 2
 	}
 		
 }
-
-
