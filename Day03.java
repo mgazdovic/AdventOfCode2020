dce93d886a6668fbf916da7ba020bccfacd703ad
MG <62848619+mgazdovic@users.noreply.github.com>
2020-12-15 00:22:00 +0100

    Update Day03.java
    Clean up

--- Day03.java
+++ Day03.java
@@ -1,38 +1,51 @@
+/**
+ * Advent Of Code 2020
+ *  >>> www.adventofcode.com/2020
+ * 
+ * This file contains solutions to day 3.
+ * 
+ * @author Mislav.Gazdovic, mislav.gazdovic@gmail.com
+ *
+ */
+
 package adventOfCode2020;
 
-import java.util.*;
+import java.util.Collection;
 
 public class Day03 {
-
-	public static int treeCount(List<String> grid, int hop_horinzontal, int hop_vertical) {
-		
-		final int PATTERN_LENGTH = grid.get(0).length();
+	
+	public static int treeCount(char[][] grid, int hop_horinzontal, int hop_vertical) {		
+		final int PATTERN_LENGTH = grid[0].length;
 		final char TREE = '#';
-		int treeCount = 0;
 		
-		for (int posX = 0, posY = 0; posY < grid.size(); posX = (posX + hop_horinzontal) % PATTERN_LENGTH, posY += hop_vertical) 
+		int treeCount = 0;
+		for (int row = 0, col = 0; row < grid.length; row += hop_vertical, col = (col + hop_horinzontal) % PATTERN_LENGTH) 
 		{
-			if (grid.get(posY).charAt(posX) == TREE) treeCount++;
+			if (grid[row][col] == TREE) treeCount++;
 		}
 	
 		return treeCount;
 	}
 	
-	private static List<String> getInput() {
-		List<String> grid = new ArrayList<String>();
-		grid.addAll(Util.getInputAsStringCollection("Inputs\\Day03.txt"));
+	private static char[][] getInput() {
+		Collection<String> input = Util.getInputAsStringCollection("Inputs\\Day03.txt");
+		char[][] grid = new char[input.size()][];
+		
+		int i = 0;
+		for(String row : input) {
+			grid[i++] = row.toCharArray();
+		}
+		
 		return grid;
 	}
 	
 	// Driver
 	public static void main(String[] args) {
+		char[][] grid = getInput();
 		
-		final List<String> grid = getInput();
+		System.out.printf("The number of trees encountered is %d.\n", treeCount(grid, 3, 1));	// PART 1
 		
-		System.out.printf("The number of trees encountered is %d.", treeCount(grid, 3, 1));	// PART 1
-		System.out.println();
-		
-		// Check the product of the following tree count results using hops:				// PART 2
+		// Check the product of the following tree count results using hops:					// PART 2
 		/*
 	    Right 1, down 1.
 	    Right 3, down 1. (This is the slope you already checked.)
